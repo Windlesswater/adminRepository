86ea1f2a2d3c89805d2272b012e2e75672938639
LMLg <1433610305@qq.com>
2021-05-29 16:28:17 +0800

    提交测试2

---
+++ src/main/java/com/wangxing/testgithub/App.java
@@ -0,0 +1,13 @@
+package com.wangxing.testgithub;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
